5df80ac7abcc96a2b45d2661144288964197e64d
josh_rockwood <joshrockwood@hotmail.com>
2024-02-29 23:17:44 -0700

    PlayerAndEnemyCollision

--- Scripts/Detections/CollisionDetection.cs
+++ Scripts/Detections/CollisionDetection.cs
@@ -1,19 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class CollisionDetection : MonoBehaviour
 {
-    public UnityEvent collisionEnterEvent, collisionExitEvent;
+    public UnityEvent collisionEnterEvent, enemyCollisionEvent, playerCollisionEvent, collisionExitEvent;
 
     private void OnTriggerEnter(Collider other)
     {
         collisionEnterEvent.Invoke();
+        EnemyCollision(other);
+        PlayerCollision(other);
     }
 
     private void OnCollisionExit()
     {
         collisionExitEvent.Invoke();
+    }
+
+    private void EnemyCollision(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemyCollisionEvent.Invoke();
+        }
+    }
+
+    private void PlayerCollision(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCollisionEvent.Invoke();
+        }
     }
 }
